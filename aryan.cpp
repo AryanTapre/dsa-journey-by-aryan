ba775c77714b4a7ab31d28fd4b8f98347ab82b8f
aryantapre <taprearyan7@gmail.com>
2025-06-14 20:23:29 +0530

    feat(fundamentals): add display-area-of-circle.cpp for area calculations

---
+++ aryan.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "🚀 Hello, world!" << std::endl;
+    return 0;
+}
---
+++ fundamentals/display-area-of-circle.cpp
@@ -0,0 +1,21 @@
+// Program to calculate the area of circle
+#include<iostream>
+using namespace std; 
+
+float calculateCircleArea(float radius) {
+    const int PI = 3.14;
+    return PI * radius * radius;
+}
+
+int main() { 
+    float radius;
+
+    cout << "enter the radius:";
+    cin >> radius;
+
+    cout << "Area of circle is : " << calculateCircleArea(radius) << endl;
+    
+    return 0;
+}
+
+
