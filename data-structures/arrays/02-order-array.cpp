b8c8f215dea294788fa8c4cd87ec397ce3f754e6
aryantapre <taprearyan7@gmail.com>
2025-07-12 17:45:25 +0530

    feat(arrays): add removeFromArray function in 02-order-array.cpp

--- data-structures/arrays/02-order-array.cpp
+++ data-structures/arrays/02-order-array.cpp
@@ -29,6 +29,23 @@ bool insertIntoArray(int* array, size_t& currentSize, const size_t capacity, con
     return true;
 }
 
+/**
+ * @brief Searches for a target within an array
+ * 
+ * @param array The array to search within.
+ * @param length The number of elements in the array.
+ * @param target The value to search for.
+ * @return The index of the target if found; -1 otherwise
+ */
+int linearSearch(const int* array, size_t currentSize, const size_t& capacity, const int target) {
+    for (size_t index = 0; index < currentSize; ++index) {
+        if (array[index] == target) {
+            return static_cast<int>(index);
+        }
+    }
+    return -1;
+}
+
 void traverseArray(const int* array, const size_t currentSize) {
     for (size_t i = 0; i < currentSize; i++) {
         cout << array[i] << " ";
@@ -36,21 +53,60 @@ void traverseArray(const int* array, const size_t currentSize) {
     cout << "\n";
 }
 
+
+bool removeFromArray(int* array, size_t& currentSize, const size_t& capacity, int target) {
+    if (!array || currentSize <= 0) {
+        // Invalid conditions: nullptr, empty array
+        return false;
+    }
+
+    // Searching whether element is present.
+    int foundIndex = linearSearch(array, currentSize, capacity, target);
+
+
+    if (foundIndex == -1) {
+        return false; // target not found.
+    }
+
+    // target found, shifting elements towards left
+    for (size_t i = foundIndex; i < currentSize - 1; ++i) {
+        array[i] = array[i + 1];
+    }
+
+    --currentSize; // shrinking currentSize by 1
+
+    return true;
+    
+}
+
+
 int main() { 
-    int array[6] = {8, 9, 16, 23};
+    int array[6] = {5,89,55,12};
     constexpr size_t capacity = 6;
     size_t currentSize = 4;
 
-    constexpr int newValue = 89;
+    constexpr int newValue = 100;
     constexpr size_t position = 0; // Test for edge case: insert at beginning 
 
-    if (insertIntoArray(NULL, currentSize, capacity, newValue, position)) {
+    if (insertIntoArray(array, currentSize, capacity, newValue, position)) {
         cout << "Insertion successful. \n";
     } else {
         cout << "Insertion failed. \n";
     }
 
     traverseArray(array, currentSize);
+    cout << "size : "<< currentSize << endl;
+
+    int target = 12;
+    if (removeFromArray(array, currentSize, capacity, target)) {
+        cout << "Remove successful, \n";
+    } else {
+        cout << "Remove failed. \n";
+    }
+
+    
+    traverseArray(array, currentSize);
+    cout << "size : " << currentSize << endl;
 
     return 0;
 }
