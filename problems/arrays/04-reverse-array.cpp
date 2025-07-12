6738ff09ff000517b532f2e08b711fd4b603a551
aryantapre <taprearyan7@gmail.com>
2025-07-13 00:29:16 +0530

    refactor(arrays): add comments to /arrays/04-reverse-array.cpp

--- problems/arrays/04-reverse-array.cpp
+++ problems/arrays/04-reverse-array.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std; 
 
+
+/**
+ * @brief reverseArray function reverse the array using Two-Pointer approach.
+ * 
+ * @param array The array to reverse with.
+ * @param currentSize Determines no of elemnts array is currently holding.
+ * @return Void - Just reverse the array elements by Swapping.
+ * 
+ */
 void reverseArray(int* array, const size_t currentSize) {
     if (!array || currentSize <= 0) {
+        // Invalid conditions: nullptr, array is empty.
         return;
     }
 
@@ -10,10 +20,12 @@ void reverseArray(int* array, const size_t currentSize) {
     int rightIndex = currentSize - 1;
 
     while (leftIndex <= rightIndex) {
+        // Swapping
         int temp = array[leftIndex];
         array[leftIndex] = array[rightIndex];
         array[rightIndex] = temp;
 
+        // Updating pointers.
         ++leftIndex;
         --rightIndex;
     }
