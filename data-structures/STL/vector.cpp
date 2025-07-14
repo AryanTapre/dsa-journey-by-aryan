163690b4deed870affdabee474b7d3e7700df11e
aryantapre <taprearyan7@gmail.com>
2025-07-14 14:14:14 +0530

    feat(STL): add vector.cpp into /data-structures/STL  A demo usage of std::vector

---
+++ data-structures/STL/vector.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include<vector>
+// #include<bits/stdc++.h>
+using namespace std; 
+
+void traverseVector(const vector<int>& v) { 
+    for (auto i : v) {
+        cout << i << " ";
+    }
+    cout << "\n";
+
+}
+
+int main() { 
+    // vector<int> v(5,1); // declared, and allocation vector of size 5, all initialized with 1.
+    vector<int> v;
+
+    v.push_back(10);
+    v.push_back(90);
+    traverseVector(v);
+    cout << v.capacity() << " " << v.size() << "\n";
+
+    v.push_back(100);
+    cout << v.capacity() << " " << v.size() << "\n"; // Doubled the capacity 
+
+    v.insert(v.begin(), 89);
+
+    traverseVector(v);
+
+    v.erase(v.begin()); 
+
+    traverseVector(v);
+    
+    v.pop_back();
+
+    traverseVector(v);
+
+    cout << "element at(1) : " << v.at(1) << "\n";
+
+    cout << "vector is empty ? :  " << v.empty() << "\n"; 
+
+    cout << "clearing entire vector : " << "\n";
+    v.clear();
+
+    cout << "vector is empty ? :  " << v.empty() << "\n"; 
+
+
+    cout << "size : " << v.size() << ", capacity : " << v.capacity() << "\n";
+
+    return 0;
+}
