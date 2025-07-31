93655b48b01cfe80876b298a7e1c5ea8360bce4c
aryantapre <taprearyan7@gmail.com>
2025-08-01 02:03:31 +0530

    feat(problems): add squareroot using binary search at /binary-search/06-sqaure-root.cpp

---
+++ problems/binary-search/06-square-root.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std; 
+
+double squareroot(const int n, int const precision) {
+  int target = n;
+  int low = 0;
+  int high = n;
+  int mid;
+  
+  while (low <= high) {
+    mid = low + (high - low) / 2;
+
+    if ((mid * mid) == target) {
+      return mid;
+    } else if ((mid * mid) < target) {
+      low = mid + 1;
+    } else {
+      // left search
+      high = mid - 1;  
+    } 
+  }
+
+  double ans = mid;
+  double step = 0.1;
+
+  for (int i = 1; i <= precision; ++i) {
+    for (double j = ans; j * j <= n; j = j + step) {
+      ans = j;
+    }
+    step = step / 10;
+  }
+
+  return ans;
+}
+
+int main() {  
+  int n = 6;
+  int precision = 3;
+
+  cout << "square root is : " << squareroot(n, precision) << endl;
+    
+  return 0;
+}
