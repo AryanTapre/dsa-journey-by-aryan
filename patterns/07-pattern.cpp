8dc8e699e85139270e02f284e6e81751228e2c8c
aryantapre <taprearyan7@gmail.com>
2025-11-24 18:28:49 +0530

    feat(pattern): add 07-pattern.cpp

---
+++ patterns/07-pattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    int row;
+    cout << "enter no of rows : ";
+    cin >> row;
+    
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < row; j++) {
+            if (i == 0 || j == 0 || j == row - i - 1 ) {
+                cout << "*"<< " ";     
+            } else {
+                cout << " " << " ";
+            }                        
+        }
+        cout << endl;               
+    }
+    
+    return 0;
+}
