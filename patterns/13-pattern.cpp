ab5c529b6deb6d190b549c054ab10aa9f7a5cc64
aryantapre <taprearyan7@gmail.com>
2025-11-26 22:30:48 +0530

    feat(pattern): add 13-pattern.cpp

---
+++ patterns/13-pattern.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    int n;
+    
+    cout << "enter value of n:";
+    cin >> n;
+
+    for (int i = 0; i < n / 2; i++) {
+        for (int j = 0; j < (n/2) - i; j++) {
+            cout << "*";    
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n / 2; i++) {
+        for (int j = 0; j < 2*i+1; j++) {
+            cout << " ";        
+        }
+        cout << endl;    
+    }
+
+    for (int i = 0; i < n / 2; i++) {
+        for (int j = 0; j < (n/2)-i; j++) {
+            cout << "*";    
+        }
+        cout << endl;    
+    }
+
+    return 0;
+}
+
+
