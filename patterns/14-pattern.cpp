11a538e98988e8ce312ea6c49843680da39198f2
aryantapre <taprearyan7@gmail.com>
2025-11-29 15:37:23 +0530

    chore(pattern): rewriiten logic for star print in 14-pattern.cpp

--- patterns/14-pattern.cpp
+++ patterns/14-pattern.cpp
@@ -9,25 +9,21 @@ int main() {
     cin >> n;
 
     for (int i = 0; i < n; i++) {
-        int starCount = i;
         for (int j = 0; j < i + 1; j++) {
             cout << i + 1;
-            if (starCount) {
-                cout << "*";
-                --starCount;
+            if ( j < i) { // means not a last character
+                cout << "*";    
             }    
         }
         cout << endl;    
     }
 
     for (int i = 0; i < n; i++) {
-        int starCount = n - i - 1;
 
         for (int j = 0; j < n - i; j++) {
             cout << n - i;
-            if (starCount) {
-                cout << "*";
-                --starCount;    
+            if ( j < n - 1) {
+                cout << "*";    
             }
         }
         cout << endl;    
